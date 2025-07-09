fda39820ae5c691cf944759de00bd03b8bdceab5
dcbroad3 <dcbroad3@gmail.com>
2025-07-09 14:34:38 +0000

    Deploying to main from @ factoriolab/factoriolab@ec650f1c0d9e15b5f2ab40390ec94d221ba115c6 🚀

---
+++ chunk-YWC45TBN.js
@@ -0,0 +1,1 @@
+import{c as n}from"./chunk-E6J6IL6M.js";import{S as r,W as i}from"./chunk-QKBXNPCR.js";var u=(()=>{class t{constructor(){this.displaySvc=r(n)}transform(e){return this.displaySvc.toBonusPercent(e)}static{this.\u0275fac=function(s){return new(s||t)}}static{this.\u0275pipe=i({name:"bonusPercent",type:t,pure:!0,standalone:!0})}}return t})();export{u as a};
